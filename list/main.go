ed66b538cc7a84515acbdab4e2f731edb0b3ccfc
chauhoangminhnguyen <chauhoangminhnguyen@gmail.com>
2024-01-10 10:03:38 +0700

    update list

--- list/main.go
+++ list/main.go
@@ -10,6 +10,11 @@ func ListTest() {
 	fmt.Println("ListTest", now)
 }
 
+func ListTest2() {
+	now := time.Now()
+	fmt.Println("ListTest2", now)
+}
+
 // define map function like js
 func Map[K, V any](s []K, transform func(K) V) []V {
 	rs := make([]V, len(s))
--- number/main.go
+++ number/main.go
@@ -1,6 +1,6 @@
 package mz_number
 
-func Min[T SignedInteger](s []T) T {
+func Min[T SignedInteger](s ...T) T {
 	r := s[0]
 	for _, v := range s[1:] {
 		if r > v { // type is number so can be compare
@@ -14,6 +14,16 @@ func MinWithCondition[T any](s []T, compare func(T, T) int) T {
 	r := s[0]
 	for _, v := range s[1:] {
 		if compare(r, v) > 0 {
+			r = v
+		}
+	}
+	return r
+}
+
+func Max[T SignedInteger](s ...T) T {
+	r := s[0]
+	for _, v := range s[1:] {
+		if r < v {
 			r = v
 		}
 	}
